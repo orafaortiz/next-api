979592309ce04ca342ca521b99ee401aed4ae3d5
Rafael Ortiz <rafaelortiz@frwk.com.br>
2022-06-18 18:39:22 -0300

    Commit to develop

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>SC São José</h1>
+      <h1>SC - São José</h1>
       <h2>Encontre no seu bairro os produtos que precisa, rápido e simples.</h2>
-      
+      <p>E ainda contribui para o desenvolvimento economico da sua cidade.</p>
     </div>
   );
 }
